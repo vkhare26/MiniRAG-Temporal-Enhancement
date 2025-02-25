ae4a27e2b5caef2f03e069925af720c00140acfe
yashshah035 <yashshah035@gmail.com>
2025-02-25 10:18:36 +0530

    fix imports

--- minirag/llm/openai.py
+++ minirag/llm/openai.py
@@ -66,7 +66,7 @@ from tenacity import (
     wait_exponential,
     retry_if_exception_type,
 )
-from lightrag.utils import (
+from minirag.utils import (
     wrap_embedding_func_with_attrs,
     locate_json_string_body_from_string,
     safe_unicode_decode,
